337d1a0e88e0e8e332e3ac1b94ff92486aa25cad
Liliana <lilianakoric@gmail.com>
2024-03-31 19:24:51 -0700

    Add TITLE = "css selector", assert actual_text

--- test_loginLK.py
+++ test_loginLK.py
@@ -3,6 +3,7 @@ import time
 USER_NAME = ("css selector", "input[data-test='username']")
 PASSWORD = ("css selector", "input[data-test='password']")
 LOGIN = ("css selector", "input[data-test='login-button']")
+TITLE = ("css selector", "span[data-test='title']")
 
 
 def test_loginLK(driver):
@@ -10,5 +11,9 @@ def test_loginLK(driver):
     driver.find_element(*USER_NAME).send_keys("standard_user")
     driver.find_element(*PASSWORD).send_keys("secret_sauce")
     driver.find_element(*LOGIN).click()
+
+    actual_text = driver.find_element(*TITLE).text
+    expected_text = "Products"
+    assert actual_text == expected_text, f"Unexpected text, expected text: {expected_text}, actual text: {actual_text}"
     time.sleep(3)
 
