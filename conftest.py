db73cc69c6ebc1e36d1ff8026716af8fb044b280
Liliana <lilianakoric@gmail.com>
2024-03-31 21:35:57 -0700

    Add   selecrors into conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from  selenium.webdriver.chrome.service import Service
 
+USER_NAME = ("css selector", "input[data-test='username']")
+PASSWORD = ("css selector", "input[data-test='password']")
+LOGIN = ("css selector", "input[data-test='login-button']")
+
 @pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
@@ -10,5 +14,9 @@ def driver():
     chrome_options.add_argument("--window-size=1440,1080")   #max window
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(*USER_NAME).send_keys("standard_user")
+    driver.find_element(*PASSWORD).send_keys("secret_sauce")
+    driver.find_element(*LOGIN).click()
     yield driver
     driver.quit()
--- test_loginLK.py
+++ test_loginLK.py
@@ -1,29 +1,27 @@
 import time
 
-USER_NAME = ("css selector", "input[data-test='username']")
-PASSWORD = ("css selector", "input[data-test='password']")
-LOGIN = ("css selector", "input[data-test='login-button']")
+# USER_NAME = ("css selector", "input[data-test='username']")  ## go to fixture conftest
+# PASSWORD = ("css selector", "input[data-test='password']")
+# LOGIN = ("css selector", "input[data-test='login-button']")
 TITLE = ("css selector", "span[data-test='title']")
 CARDS = ("css selector", "div[data-test='inventory-item']")
 
 
-
 def test_loginLK(driver):
-    driver.get("https://www.saucedemo.com/")
-    driver.find_element(*USER_NAME).send_keys("standard_user")
-    driver.find_element(*PASSWORD).send_keys("secret_sauce")
-    driver.find_element(*LOGIN).click()
-
+    # driver.get("https://www.saucedemo.com/")     ## go to fixture
+    # driver.find_element(*USER_NAME).send_keys("standard_user")
+    # driver.find_element(*PASSWORD).send_keys("secret_sauce")
+    # driver.find_element(*LOGIN).click()
     actual_text = driver.find_element(*TITLE).text
     expected_text = "Products"
     assert actual_text == expected_text, f"Unexpected text, expected text: {expected_text}, actual text: {actual_text}"
     time.sleep(3)
 
 def test_loginLK(driver):
-    driver.get("https://www.saucedemo.com/")
-    driver.find_element(*USER_NAME).send_keys("standard_user")
-    driver.find_element(*PASSWORD).send_keys("secret_sauce")
-    driver.find_element(*LOGIN).click()
+    # driver.get("https://www.saucedemo.com/")           # go to fixture
+    # driver.find_element(*USER_NAME).send_keys("standard_user")
+    # driver.find_element(*PASSWORD).send_keys("secret_sauce")
+    # driver.find_element(*LOGIN).click()
     expected_len = 6
     cards = driver.find_elements(*CARDS)
     assert len(cards) == expected_len, f"Expected: {expected_len}, actual: {len(cards)}"
