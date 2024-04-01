4fdebb4d10c6ec2c4823877c4f46fbd3399c0cbb
Liliana <lilianakoric@gmail.com>
2024-03-31 22:02:57 -0700

    Create base_page.py,  def __init__(self, driver,url):, def open(self):.class LoginLocators, login_locatots.py,

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,2 @@
 
 
-class LoginLocators:
-    USER_NAME = ("css selector", "input[data-test='username']")
-    PASSWORD = ("css selector", "input[data-test='password']")
-    LOGIN = ("css selector", "input[data-test='login-button']")
