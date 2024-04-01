ab18fc58041fa12b0de54d951da804ccb80a859f
Liliana <lilianakoric@gmail.com>
2024-03-31 19:32:59 -0700

    Add  CARDS "css selector",

--- test_loginLK.py
+++ test_loginLK.py
@@ -4,6 +4,8 @@ USER_NAME = ("css selector", "input[data-test='username']")
 PASSWORD = ("css selector", "input[data-test='password']")
 LOGIN = ("css selector", "input[data-test='login-button']")
 TITLE = ("css selector", "span[data-test='title']")
+CARDS = ("css selector", "div[data-test='inventory-item']")
+
 
 
 def test_loginLK(driver):
@@ -13,7 +15,19 @@ def test_loginLK(driver):
     driver.find_element(*LOGIN).click()
 
     actual_text = driver.find_element(*TITLE).text
-    expected_text = "Products!"
+    expected_text = "Products"
     assert actual_text == expected_text, f"Unexpected text, expected text: {expected_text}, actual text: {actual_text}"
     time.sleep(3)
+
+def test_loginLK(driver):
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(*USER_NAME).send_keys("standard_user")
+    driver.find_element(*PASSWORD).send_keys("secret_sauce")
+    driver.find_element(*LOGIN).click()
+
+    cards = driver.find_elements(*CARDS)
+    for i in cards:
+        print(i.text)
+
+
 
