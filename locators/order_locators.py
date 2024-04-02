4413011f42a17d663ac94bef4127039d33b43b7c
Liliana <lilianakoric@gmail.com>
2024-04-01 21:01:06 -0700

    Add  @pytest.mark.parametrize("lst_data", OrderData.user_data), def fill_field(self, first_name, last_name, zip_code) - in order_data.py

--- locators/order_locators.py
+++ locators/order_locators.py
@@ -1,4 +1,11 @@
 
 
 class OrderLocators:
-    pass
+    FIRST_NAME = ("css selector", "input[data-test='firstName']")
+    LAST_NAME = ("css selector", "input[data-test='lastName']")
+    ZIP = ("css selector", "input[data-test='postalCode']")
+    CONTINUE_BTN = ("css selector", "input[id='continue']")
+    FINISH_BTN = ("css selector", "button[id='finish']")
+    SUCCESSFUL_ORDER = ("css selector", "h2[data-test='complete-header']")
+    ERROR_MESSAGE = ("css selector", "h3[data-test='error']")
+
---
+++ src/order_data.py
@@ -0,0 +1,6 @@
+class OrderData:
+    first_name_error_message = " Error: First Name is required"
+    last_name_error_message = " Error: Last Name is required"
+    postal_code_error_message = " Error: Postal Code is required"
+
+    user_data = [["", " Ivso", "54321"],["Den", "", "54321"], ["Den", " Ivso", ""]]
