9d44dacc860c09efdbeaf8339d20e5db6e43f3ee
Liliana <lilianakoric@gmail.com>
2024-04-05 13:34:33 -0700

    Updated and improved class OrderData in order_data.py

--- pages/order_page.py
+++ pages/order_page.py
@@ -15,12 +15,10 @@ class OrderPage(BasePage):
         self.click_to_element(self.order_locators.FINISH_BTN)
         return self.get_text(self.order_locators.SUCCESSFUL_ORDER)
 
-
     def order_with_wrong_credential(self, lst_data):
         self.add_card_to_cart()
         self.fill_field( lst_data[0], lst_data[1], lst_data[2])
         return self.get_text(self.order_locators.ERROR_MESSAGE)
-
 
     def add_card_to_cart(self):
         self.element_is_clickable(self.main_locators.SAUCE_LABS_BACKPACK).click()
--- src/order_data.py
+++ src/order_data.py
@@ -1,7 +1,4 @@
 class OrderData:
-    # first_name_error_message = "Error: First Name is required"
-    # last_name_error_message = "Error: Last Name is required"
-    # postal_code_error_message = "Error: Postal Code is required"
 
     user_data = [["", " Ivso", "54321", "Error: First Name is required"],
                  ["Den", "", "54321", "Error: Last Name is required"],
