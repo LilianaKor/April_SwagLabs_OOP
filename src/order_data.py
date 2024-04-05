d147f62cfff43097ff96428abfbc52e504e3c350
Liliana <lilianakoric@gmail.com>
2024-04-04 22:05:00 -0700

    Add   data = OrderData,  assert self.data.successful_massage in test_order.py

--- src/order_data.py
+++ src/order_data.py
@@ -6,3 +6,6 @@ class OrderData:
     user_data = [["", " Ivso", "54321", "Error: First Name is required"],
                  ["Den", "", "54321", "Error: Last Name is required"],
                  ["Den", " Ivso", "", "Error: Postal Code is required"]]
+
+    user_data_with_valid_credential = ["Den", " Ivso", "54321"]
+    successful_massage = "Thank you for your order!"
