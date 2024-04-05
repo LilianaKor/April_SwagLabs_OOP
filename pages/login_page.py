669b022fa788743799ed3f311e67e535fac3ee97
Liliana <lilianakoric@gmail.com>
2024-04-05 13:28:55 -0700

    Delete  # def login(self):  # go to base page

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,9 +6,3 @@ class LoginPage(BasePage):
     locators = LoginLocators()
     user = UserData()
 
-    # def login(self):  # go to base page
-    #     self.driver.find_element(*self.locators.USER_NAME).send_keys(self.user.standard_user)
-    #     self.driver.find_element(*self.locators.PASSWORD).send_keys(self.user.password)
-    #     self.driver.find_element(*self.locators.LOGIN).click()
-
-# pass
