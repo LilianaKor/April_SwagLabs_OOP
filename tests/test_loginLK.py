7e0b6b434d2d15b977394a2fd9b72d16e4597aa3
Liliana <lilianakoric@gmail.com>
2024-04-05 13:44:32 -0700

    Updated and improved test_loginLK.py: driver.find... -> go to fixture . Deleted   # cards = driver.find_elements(*CARDS)

--- tests/test_loginLK.py
+++ tests/test_loginLK.py
@@ -3,13 +3,13 @@ import time
 from locators.main_locators import Mainlocators
 from pages.login_page import LoginPage
 from src.urls import Urls
-
 from pages.login_page import LoginPage
 from src.urls import Urls
 
 class TestLogin():
     url = Urls()
     main_locators = Mainlocators()
+
     def test_loginLK(self, driver):
         page = LoginPage(driver, self.url.base_url)
         page.open()
@@ -19,19 +19,13 @@ class TestLogin():
         expected_text = "Products"
         assert actual_text == expected_text, \
             f"Unexpected text, expected text: {expected_text}, actual text: {actual_text}"
-        #time.sleep(3)
 
     def test_login1LK(self, driver):
         page = LoginPage(driver, self.url.base_url)
         page.open()
         page.login()
-        # driver.get("https://www.saucedemo.com/")           # go to fixture
-        # driver.find_element(*USER_NAME).send_keys("standard_user")
-        # driver.find_element(*PASSWORD).send_keys("secret_sauce")
-        # driver.find_element(*LOGIN).click()
         expected_len = 6
         cards = page.get_length(self.main_locators.CARDS)
-        # cards = driver.find_elements(*CARDS)
         assert cards == expected_len, f"Expected: {expected_len}, actual: {cards}"
 
 
